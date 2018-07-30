a68b6544bd3bb40be034e26460b5acc86d7c7202
Christy Welch <christywelch2@Christys-MacBook-Pro.local>
2018-07-30 11:51:51 -0500

    scene setup

---
+++ Assets/Pantry_Party/Scripts/Billboard.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace A07Examples
+{
+    public class Billboard : MonoBehaviour
+    {
+
+        void Update()
+        {
+            transform.LookAt(Camera.main.transform);
+        }
+    }
+}
